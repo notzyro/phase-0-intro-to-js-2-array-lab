4f1be065cae680674a5cd5b88152dc08272c138b
Edward Angelo Salas <zyro670@gmail.com>
2022-03-25 22:40:31 -0400

    commit tree bdb58cd8f7a6f931a08fc2ba137c84c42977bcfa

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = [ "Milo", "Otis", "Garfield"];
+
+function beforeEach() {
+    cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield");
+};
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(cats);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat() {
+    const newcats = [...cats, "Broom"];
+    return newcats;
+}
+
+function prependCat() {
+    const oldcats = ["Arnold", ...cats];
+    return oldcats;
+}
+
+function removeLastCat() {
+    const newcat = cats.slice(0, cats.length - 1);
+    return newcat;
+}
+
+function removeFirstCat() {
+    const firstcat = cats.slice(1);
+    return firstcat;
+}
